fc2726cefa4c8a12dc16b000a9b977afbacf1967
Kavya Gupta <kavygupta0605@gmail.com>
2026-01-01 23:26:45 +0530

    Add files via upload

---
+++ Day1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class ArrayOps {
+private:
+    int arr[100];
+    int n;
+
+public:
+    void input() {
+        cout << "Enter size of array: ";
+        cin >> n;
+
+        cout << "Enter " << n << " elements: ";
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+    }
+
+    void removeDuplicates() {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    for (int k = j; k < n - 1; k++) {
+                        arr[k] = arr[k + 1];
+                    }
+                    n--;
+                    j--;
+                }
+            }
+        }
+    }
+
+    void display() {
+        cout << "Array after removing duplicates: ";
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    ArrayOps obj;
+    obj.input();
+    obj.removeDuplicates();
+    obj.display();
+    return 0;
+}
